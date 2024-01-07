2d51a716a12216f4eafa43fc6e45e41de498ee78
Marco Zocca <marco.zocca@unfoldml.com>
2024-01-07 11:32:04 +0100

    plot div id can be selected

--- htmx-plotly.js
+++ htmx-plotly.js
@@ -3,30 +3,22 @@
 htmx.defineExtension('htmx-plotly', {
     transformResponse : (text, xhr, elt) => {
 
-        // const dataCurr = window['plotlyTestData']
-        // console.log('htmx-plotly: plotlyTestData:' + JSON.stringify(dataCurr) ) // debug
-
-        const dataNew = JSON.parse(text)
-        // console.log('htmx-plotly: new data: ' + JSON.stringify(dataNew))
-        // window['plotlyTestData'] = dataNew
-        Plotly.restyle('my-plot', dataNew);
+        var plotEl = htmx.closest(elt, "[plot-id]"); // closest including div element
+        if (plotEl) {
+            const dataNew = JSON.parse(text)
+            const plotId = plotEl.getAttribute('plot-id');  // lookup value of ? in < .. plot-id="?">
+            var plotDiv = htmx.find("#" + plotId); // div element pointed at
+            if (plotDiv) {
+                // https://plotly.com/javascript/plotlyjs-function-reference/#plotlyrestyle 
+                Plotly.restyle(plotId, dataNew)
+            } else {
+                throw "Cannot find plot id: " + plotId
+            }
+        } else {
+            console.log('No plot-id attribute defined')
+        }
 
         return ''
-
-        // var plotIdAttr = htmx.closest(elt, "[plot-id]"); // closest including div element
-        // if (plotIdAttr) {
-        //     var data = JSON.parse(text);
-        //     var plotId = plotIdAttr.getAttribute('plot-id'); // < .. plot-id="?"> value of ?
-            
-        //     var plotDiv = htmx.find("#" + plotId); // div element pointed at
-        //     if (plotDiv) {
-        //         // https://plotly.com/javascript/plotlyjs-function-reference/#plotlyrestyle 
-        //         Plotly.restyle(plotId, 'y', [data]);
-
-        //     } else {
-        //         throw "Cannot find plot id:" + plotDiv
-        //     }
-        // }
 
     }
 }
